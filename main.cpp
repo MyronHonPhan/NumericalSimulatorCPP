ba5b7f4bed15a0d80fb06b6ce7b0af77a53a509f
Myron Phan <myron.phan@gmail.com>
2021-10-23 10:17:35 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+class Rocket {
+public:
+  Rocket() {
+    
+  }
+private:
+  int mass; // Newtons
+  int length; // Meters
+
+
+};
+
+
+
+float RK4() {
+
+
+}
+
+class Dynamics {
+public:
+  Dynamics () {
+
+  }
+private:
+  double phi;
+  double theta;
+  double psi;
+  double x;
+  double y;
+  double z;
+  
+};
+
+class Environment {
+public:
+  Environment() {
+
+  }
+};
+
+int main() {
+  std::cout << "Hello World!\n";
+}
